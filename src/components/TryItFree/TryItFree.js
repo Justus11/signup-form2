d5f9e29eae45805f2b88fa4a20326918457704ca
Justus11 <57481057+Justus11@users.noreply.github.com>
2021-12-17 11:59:53 +0200

    j

---
+++ src/components/TryItFree/TryItFree.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './TryItFree.css'
+
+const TryItFree = () => {
+    return (
+        <div>
+            "Hello world"
+        </div>
+    )
+}
+export default TryItFree;
